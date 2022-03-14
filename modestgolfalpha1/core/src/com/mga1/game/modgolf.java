611ff426aec86916829032565c09f9a345bade7c
Liam <91745503+crispyliam@users.noreply.github.com>
2022-03-14 15:52:51 +0100

    Graphical systems etc.

--- modestgolfalpha1/core/src/com/mga1/game/modgolf.java
+++ modestgolfalpha1/core/src/com/mga1/game/modgolf.java
@@ -1,7 +1,10 @@
 package com.mga1.game;
 
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -12,55 +15,87 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import jdk.jfr.internal.settings.PeriodSetting;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.utils.Array;
 
 public class modgolf extends ApplicationAdapter {
 
-	//buttons and menu
-	private Stage Stage;
-	private Texture playbutton;
-	private TextureRegion playzone;
-	private TextureRegionDrawable playdraw;
-	private ImageButton button;
-
-	//other stuff
-
-	public Texture funnyface;
-
+	//camera
 	public PerspectiveCamera cam;
-	public ModelBatch modelBatch;
-	public Model redpill;
-	public Model ball;
-	public ModelInstance instance;
-	public Environment universe;
 	public CameraInputController camController;
-	
+
+	//models
+	public ModelInstance inst;
+	public ModelBatch modelBatch;
+	public AssetManager assets;
+	public Array<ModelInstance> instances = new Array<ModelInstance>();
+
+	//skybox etc
+	public Environment environment;
+
+	//states
+	public boolean loading;
+
 	@Override
 	public void create () {
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(1f, 1f, 1f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
+
+		assets = new AssetManager();
+		assets.load("data/gun.obj", Model.class);
+		loading = true;
+	}
+
+	private void doneLoading() {
+		Model ship = assets.get("data\\C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\models\\gun.obj", Model.class);
+		ModelInstance shipInstance = new ModelInstance(ship);
+		instances.add(shipInstance);
+		loading = false;
 	}
 
 	@Override
 	public void render () {
+		if (loading && assets.update())
+			doneLoading();
+		camController.update();
 
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+		modelBatch.begin(cam);
+		modelBatch.render(instances, environment);
+		modelBatch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
-
+		modelBatch.dispose();
+		instances.clear();
+		assets.dispose();
 	}
 
-	@Override
-	public void resume ()  {
-
+	public void resume () {
 	}
 
-	@Override
+	public void resize (int width, int height) {
+	}
+
 	public void pause () {
-
 	}
 }
