13a33b1cabf62b1a3529b596bcbaa14183b0cbc7
spism <91736528+spism@users.noreply.github.com>
2022-03-15 11:40:50 +0100

    Sand pits + acceleration eq checked

--- modestgolfalpha1/core/PhysicsEngine.java
+++ modestgolfalpha1/core/PhysicsEngine.java
@@ -4,7 +4,9 @@ import java.util.Stack;
 public class PhysicsEngine
 {
     double firstX, firstY, targetX, targetY, targetRadius;
+    double sandX1, sandX2, sandY1, sandY2;
     double grassKinetic, grassStatic;
+    double sandKinetic, sandStatic;
     String heightProfile;
     final double h = 0.1;
     final double g = 9.81;
@@ -45,6 +47,20 @@ public class PhysicsEngine
                 {
                     heightProfile = line;
                     //getHeight(heightProfile,firstX,firstY);
+                }
+                else if(lineIndex == 4)
+                {
+                    sandX1 = Double.parseDouble(arr[3]);
+                    sandX2 = Double.parseDouble(arr[5]);
+                    arr = br.readLine().split(" ");
+                    sandY1 = Double.parseDouble(arr[3]);
+                    sandY2 = Double.parseDouble(arr[5]);
+                }
+                else if(lineIndex == 5)
+                {
+                    sandKinetic = Double.parseDouble(arr[3]);
+                    arr = br.readLine().split(" ");
+                    sandStatic = Double.parseDouble(arr[3]);
                 }
                 lineIndex++;
             }
@@ -116,24 +132,37 @@ public class PhysicsEngine
 
     /**
      *
-     * @param initX
-     * @param initY
-     * @param initSpeedX in m/s
-     * @param initSpeedY in m/s
-     * @param timeframe is how many seconds per frame
+     * @param initX in meters
+     * @param initY in meters
+     * @param initSpeedX in meters per second
+     * @param initSpeedY in meters per second
      */
     public void updateVector(double initX, double initY, double initSpeedX, double initSpeedY)
     {
         double kineticDenominator = Math.sqrt(initSpeedX * initSpeedX + initSpeedY * initSpeedY);
-        double newSpeedX = initX + h * initSpeedX;
-        double newSpeedY = initY + h * initSpeedY;
-        double xAccel =  -g * ((Math.abs(getHeight(heightProfile,initX,initY) - getHeight(heightProfile,newSpeedX, newSpeedY)))/(h * initSpeedX)) - grassKinetic * g * (initSpeedX / kineticDenominator);
-        double yAccel = -g * ((Math.abs(getHeight(heightProfile,initX,initY) - getHeight(heightProfile,newSpeedX, newSpeedY)))/(h * initSpeedY)) - grassKinetic * g * (initSpeedY / kineticDenominator);
+        double newX = initX + h * initSpeedX;
+        double newY = initY + h * initSpeedY;
+        double kineticCoeff = sandX1 < initX && initX < sandX2 && sandY1 < initY && initY < sandY2 ? sandKinetic : grassKinetic;
+        double xAccel = -g * ((Math.abs(getHeight(heightProfile,initX,initY) - getHeight(heightProfile,newX, newY))) / (h * initSpeedX)) - kineticCoeff * g * (initSpeedX / kineticDenominator);
+        double yAccel = -g * ((Math.abs(getHeight(heightProfile,initX,initY) - getHeight(heightProfile,newX, newY))) / (h * initSpeedY)) - kineticCoeff * g * (initSpeedY / kineticDenominator);
         double[] newStateVector = {initSpeedX, initSpeedY, xAccel, yAccel};
         for(int i = 0; i < stateVector.length; i++)
         {
             stateVector[i] = stateVector[i] + h * newStateVector[i];
         }
+    }
+
+    public boolean atRest(double x, double y)
+    {
+        double derivative = (getHeight(heightProfile,x,y) - getHeight(heightProfile,x + 0.0000001, y + 0.0000001)) / 0.0000001;
+        if(grassStatic > Math.sqrt(derivative * derivative + derivative * derivative)) return true;
+        else return false;
+    }
+
+    public void runSimulation(double initX, double initY, double initSpeedX, double initSpeedY)
+    {
+        while(stateVector[0] != 0 && stateVector[1] != 0) updateVector(initX,initY,initSpeedX,initSpeedY);
+        if(stateVector[0] == 0 && stateVector[1] == 0) while(stateVector[2] != 0 && stateVector[3] != 0) updateVector(stateVector[0],stateVector[1],stateVector[2],stateVector[3]);
     }
 
     public static void main(String[] args)
