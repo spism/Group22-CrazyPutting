be64108e880b30801da71450298324982edeaedb
JesseSte <91744132+JesseSte@users.noreply.github.com>
2022-05-10 12:44:11 +0200

    Add files via upload

--- modestgolfalpha1/core/src/com/mga1/game/modgolf.java
+++ modestgolfalpha1/core/src/com/mga1/game/modgolf.java
@@ -40,7 +40,7 @@ import com.badlogic.gdx.Input.Keys;
 public class modgolf extends Game{
 
 	//phys
-	public PhysicsEngine phys = new PhysicsEngine("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\core\\src\\com\\mga1\\game\\example_inputfile.txt");
+	public PhysicsEngine phys = new PhysicsEngine("modestgolfalpha1\\core\\src\\com\\mga1\\game\\example_inputfile.txt");
 
 	//camera
 	public PerspectiveCamera cam;
@@ -75,10 +75,10 @@ public class modgolf extends Game{
 	public Model createstuff(){
 
 
-		phys = new PhysicsEngine("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\core\\src\\com\\mga1\\game\\example_inputfile.txt");
+		phys = new PhysicsEngine("modestgolfalpha1\\core\\src\\com\\mga1\\game\\example_inputfile.txt");
 
 		grass = new Material();
-		grass.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\Textures\\grass texture.png"))));
+		grass.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("modestgolfalpha1\\Textures\\grass texture.png"))));
 
 		final int divisions = 128;
 		final int dimension = 200;
@@ -119,24 +119,24 @@ public class modgolf extends Game{
 		Gdx.graphics.setContinuousRendering(false);
 
 		Material grass2 = new Material();
-		grass2.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\Textures\\grass texture.png"))));
+		grass2.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("modestgolfalpha1\\Textures\\grass texture.png"))));
 		bbuilder = new ModelBuilder();
 
 		testfloor = createstuff();
 		Material water = new Material();
-		water.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\Textures\\water-pool.jpg"))));
+		water.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("modestgolfalpha1\\Textures\\water-pool.jpg"))));
 		Material gballs = new Material();
-		gballs.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\Textures\\Golfball.png"))));
+		gballs.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture(Gdx.files.internal("modestgolfalpha1\\Textures\\Golfball.png"))));
 		Model golfball = bbuilder.createSphere(3,3,3,32,32,gballs,Usage.Position | Usage.Normal);
 		Model waterp = bbuilder.createBox(128f,1f,128f, water, Usage.Position | Usage.Normal);
 		Model skyfix = bbuilder.createBox(1000,1000,1000, water, Usage.Position | Usage.Normal);
 		Model skyball = bbuilder.createSphere(-9000,-9000,-9000,64,64,water,Usage.Position | Usage.Normal);
 		Model flagpole = new Model();
 		Model grassp = bbuilder.createBox(256f,1f,256f,grass2, Usage.Position | Usage.Normal);
-		flagpole = new G3dModelLoader(new JsonReader()).loadModel(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\models\\flagpole.g3dj"));
+		flagpole = new G3dModelLoader(new JsonReader()).loadModel(Gdx.files.internal("modestgolfalpha1\\models\\flagpole.g3dj"));
 
-		sky = new G3dModelLoader(new JsonReader()).loadModel(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\models\\simplesky.g3dj"));
-		putter = new G3dModelLoader(new JsonReader()).loadModel(Gdx.files.internal("C:\\Users\\liams\\Documents\\Java Projects\\Shithole\\Group22-phase-one\\modestgolfalpha1\\models\\putterfix.g3dj"));
+		sky = new G3dModelLoader(new JsonReader()).loadModel(Gdx.files.internal("modestgolfalpha1\\models\\simplesky.g3dj"));
+		putter = new G3dModelLoader(new JsonReader()).loadModel(Gdx.files.internal("modestgolfalpha1\\models\\putterfix.g3dj"));
 
 		ModelInstance penaltywater = new ModelInstance(waterp,64, -0.8f, 192);
 		ModelInstance penaltygrass = new ModelInstance(grassp,128, -8, 128);
@@ -180,7 +180,6 @@ public class modgolf extends Game{
 
 	@Override
 	public void render () {
-		int i = 0;
 		camController.update();
 
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -196,22 +195,20 @@ public class modgolf extends Game{
 					cam.position.set(-64f, 50f, 256f);
 					cam.lookAt(golfbol.transform.getTranslation(new Vector3()));
 					cam.update();
-					System.out.println(phys.function(phys.stateVector[0], phys.stateVector[1]));
-					phys.runSimulation(5, 0, 1);
+					System.out.println("XPOS: " + phys.stateVector[0] + " YPOS: " + phys.stateVector[1]);
+					System.out.println("Height: " + phys.function(phys.stateVector[0], phys.stateVector[1]));
+					int i = 0;
+					while(i < 100){
+					phys.runSimulation(3, 0, 2);
+					i = i + 1;
+					}
 					moveBall((float) phys.stateVector[0], (float) phys.stateVector[1]);
 
 					if (phys.targetX - phys.targetRadius < phys.stateVector[0] && phys.stateVector[0] < phys.targetX + phys.targetRadius && phys.targetY - phys.targetRadius < phys.stateVector[1] && phys.stateVector[1] < phys.targetY + phys.targetRadius) {
 						System.out.println("You Win!");
 					}
-					if (phys.function(phys.stateVector[0], phys.stateVector[1]) <= -0.8) {
+					if (phys.stateVector[2] == 0 && phys.stateVector[3] == 0) {
 						System.out.println("You Suck!");
-					}
-					if ((phys.stateVector[2]) < 0.1 && (phys.stateVector[3]) < 0.1) {
-						while (!phys.atRest(phys.stateVector[0], phys.stateVector[1])) {
-							phys.runSimulation(1, 0, 0);
-
-						}
-
 					}
 
 				}
